20e8ff287243cf4b81ae2a44cc38579a89116911
hn1f <hn1f@proton.me>
2025-12-09 19:43:22 +0100

    remove useless shellscript

--- export_gen.sh
+++
@@ -1,7 +0,0 @@
-# TODO: add hints/ordinals into these exports aswell
-
-grep "Function Name" $1 |
-awk '{print $4}' |
-while read h; do
-	echo "#pragma comment(linker, \"/export:$h=$2.$h\")"
-done > exports.h
